0adfab164c9f05db265c86df5dca6d48755e6112
FTC23368 <malihajburney@gmail.com>
2024-05-11 23:39:31 -0700

    Setup part 2

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
 import streamlit as st 
+
+def show_navigation():
+    with st.container(border=True):
+        col1,col2,col3,col4=st.columns(4)
+        col1.page_link("Home.py", label="Home", icon="🏠")
+        col2.page_link("pages/0_upload_pdf.py", label="Upload PDF", icon="1️⃣")
+        col3.page_link("pages/1_chat_with_AI.py", label="Chat", icon="2️⃣")
+        col4.page_link("pages/2_retreival_augmented_chat.py", label="RAG", icon="🌎")
